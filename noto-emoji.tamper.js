d028c1af6101e6ab61c41223c46abc96ed3842d3
bitten 1up <seant9140@gmail.com>
2022-09-15 15:31:05 -0500

    Update noto-emoji.tamper.js

--- noto-emoji.tamper.js
+++ noto-emoji.tamper.js
@@ -4,7 +4,7 @@
 // @version      1.0
 // @description  use noto monochrome emoji everywhere
 // @author       Bitten2up
-// @match        *://*
+// @match        *://*/*
 // @icon         https://github.com/litten2up/bittens-adventure/blob/raylib-rewrite-because-yes/assets/window.png?raw=true
 // @grant        none
 // ==/UserScript==
@@ -20,7 +20,6 @@ function addGlobalStyle(css) {
 (function() {
     'use strict';
     // Your code here...
-    addGlobalStyle(`@font-face {font-family: 'Noto Sans Emoji';src: url('https://raw.githubusercontent.com/litten2up/noto-emoji-script/main/Noto-Sans-emoji.ttf')format('truetype');
-    addGlobalStyle("@import url('https://fonts.googleapis.com/css2?family=Noto+Emoji:wght@500&display=swap');");
-    addGlobalStyle("*{font-family: 'Noto Emoji', sans-serif !important;}");
+    addGlobalStyle(`@font-face {font-family: 'Noto Sans Emoji';src: url('https://raw.githubusercontent.com/litten2up/noto-emoji-script/main/Noto-Sans-emoji.ttf')format('truetype');`);
+    addGlobalStyle("*{font-family: 'Noto Sans emoji', sans-serif !important;}");
 })();
